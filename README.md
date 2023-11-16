7fd708fc5d6990d70b7d32ac727aca4968c7d563
Raymond Wong <1049204910@qq.com>
2023-11-16 22:25:18 +0800

    add MSP implementation

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # MSP
+
+A PyTorch implementation of _[TCI 2023 paper](https://ieeexplore.ieee.org/document/10034834), "Multi-Stream Progressive Restoration for Low-Light Light Field Enhancement and Denoising"_
+
+> This repo is _NOT_ the official implementation of the paper. For the official implementation, visit [here](https://github.com/shuozh/MSP).
+
+## Quick Start
+
+First, download the [L3F dataset](https://mohitlamba94.github.io/L3Fnet/) and symlink it to the project root. The project directory structure should resemble:
+
+```
+|-- L3FNet
+    |-- L3F-dataset
+        |-- jpeg
+            |-- train
+            |-- test
+    |-- train.py
+    |-- eval.py
+    ...
+```
+
+Modify the `split` item in `config.toml` to specify the subset for training/evaluation/testing:
+
+``````
+[data]
+split = '20'   # should be '20', '50' or '100'
+``````
+
+To start training or testing, execute the following commands:
+
+```sh
+# training
+python train.py config.toml
+
+# testing
+python eval.py config.toml --ckpt ${CKPT_PATH}
+```
---
+++ train.py
@@ -0,0 +1,147 @@
+
+import argparse
+import logging
+import os
+from collections import defaultdict
+from pprint import pformat
+
+# from lib.pytorch_ssim import SSIM
+import piq
+import torch
+import torch.nn.functional as F
+from torch.optim import Adam
+from torch.optim.lr_scheduler import CosineAnnealingLR
+from torch.utils.data import DataLoader
+from torchvision.utils import save_image
+from tqdm import tqdm
+
+import utils
+from dataset import L3FDataset, repeater
+from models import LFEn_s3
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('config', type=str, help='Path to configuration')
+    parser.add_argument('--resume_from', type=str, help='Resume training from existing checkpoint')
+    parser.add_argument('--save_images', action='store_true', help='Dump predicted images')
+    parser.add_argument('--no_save_images', dest='save_images', action='store_false')
+    parser.set_defaults(save_images=True)
+    parser.add_argument('--cpu', action='store_true')
+    return parser.parse_args()
+
+
+def main():
+    args = parse_arguments()
+    config = utils.parse_config(args.config)
+    env = utils.init_env(args, config)
+    logging.info(f'using config file:\n{pformat(config)}')
+    logging.info(f'using device {env.device}')
+
+    if env.use_wandb:
+        import wandb
+
+    model = LFEn_s3(n_view=config.model.resolution).to(env.device)
+    optimizer = Adam(model.parameters(), lr=config.optim.base_lr)
+    scheduler = CosineAnnealingLR(optimizer, T_max=config.optim.num_iters, eta_min=config.optim.min_lr)
+
+    train_dataset = L3FDataset(config.data, mode='train', memorize=True)
+    train_loader = DataLoader(train_dataset, batch_size=config.optim.batch_size,
+                              shuffle=True, num_workers=config.env.num_workers)
+    train_loader = repeater(train_loader)  # infinite sampling
+
+    val_dataset = L3FDataset(config.data, mode='test', memorize=False)
+    val_loader = DataLoader(val_dataset, batch_size=1, shuffle=False, num_workers=0)
+
+    val_metrics = [utils.init_metrics(it) for it in config.metrics]
+    primary_val_metric_idx = next((i for i, m in enumerate(config.metrics) if m.primary == True), 0)
+    primary_metric_best = -999  # assume that the higher, the better
+
+    start_iter = 1
+    if args.resume_from:
+        logging.info(f'resume training from {args.resume_from}')
+        ckpt = torch.load(args.resume_from, map_location=env.device)
+        model.load_state_dict(ckpt['model_state_dict'])
+        optimizer.load_state_dict(ckpt['optimizer_state_dict'])
+        start_iter = int(ckpt['start_iter'])
+
+    acc_loss_dict = defaultdict(lambda: 0)
+    for iteration in tqdm(range(start_iter, config.optim.num_iters+1), dynamic_ncols=True, desc='Training'):
+        model.train()
+        data = next(train_loader)
+        lq, gt = data['lq'].to(env.device), data['gt'].to(env.device)
+        out1, out2, out3 = model(lq)
+
+        l1_loss = 0.05 * F.l1_loss(out1, gt) + 0.1 * F.l1_loss(out2, gt) + F.l1_loss(out3, gt)
+        ssim_loss = 1 - piq.ssim(out3.squeeze(0).clamp(0.0, 1.0), gt.squeeze(0), downsample=False)
+        loss = l1_loss + ssim_loss
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        scheduler.step()
+
+        acc_loss_dict['l1_loss'] += l1_loss.item()
+        acc_loss_dict['ssim_loss'] += ssim_loss.item()
+
+        if iteration % len(train_dataset) == 0:
+            logging.debug(f'[Iter {iteration}] ' +
+                          ', '.join([f'{name}: {val:.3f}' for name, val in acc_loss_dict.items()]))
+            if env.use_wandb:
+                wandb.log({'iter': iteration, **acc_loss_dict})
+            acc_loss_dict.clear()
+
+        if iteration % config.optim.eval_step == 0:
+            model.eval()
+            with torch.inference_mode():
+                for data in tqdm(val_loader, leave=False, dynamic_ncols=True, desc=f'Evaluating'):
+                    lq, gt, stem = (
+                        data['lq'].to(env.device),
+                        data['gt'].to(env.device).squeeze(0),
+                        data['stem'][0]
+                    )
+                    out = model(lq)[-1].squeeze(0)
+
+                    # crop back to original shape
+                    h, w = gt.shape[-2:]
+                    out = out[..., :h, :w]
+
+                    quant_out = out.mul(255).add_(0.5).clamp_(0, 255).to('cpu', torch.uint8)
+                    quant_gt = gt.mul(255).add_(0.5).clamp_(0, 255).to('cpu', torch.uint8)
+
+                    for (_, metric) in val_metrics:
+                        metric.update(quant_out.float(), quant_gt.float())
+
+                    if args.save_images:
+                        save_path = os.path.join(env.visual_dir(iter=iteration), f'{stem}.png')
+                        save_image(out, save_path, nrow=5, padding=0, normalize=False)
+
+                val_results = {}
+                for i, (name, metric) in enumerate(val_metrics):
+                    metric_val = metric.compute()
+                    val_results[name] = metric.compute()
+
+                    if i == primary_val_metric_idx and metric_val > primary_metric_best:
+                        primary_metric_best = metric_val
+                        ckpt_path = os.path.join(env.save_dir, 'best.pth')
+                        utils.save_state_dict(model, optimizer, iteration, ckpt_path)
+                        logging.debug(f'save checkpoint to {ckpt_path} with {name}={metric_val}')
+
+                    # reset internal state such that metric ready for new data
+                    metric.reset()
+
+                if env.use_wandb:
+                    wandb.log({'iter': iteration, **val_results})
+
+                logging.debug(f'[Iter {iteration}] ' +
+                              '; '.join([f'{name} {val:.3f}' for (name, val) in val_results.items()]))
+
+        if config.optim.save_step and iteration % config.optim.save_step == 0:
+            utils.save_state_dict(model, optimizer, iteration, os.path.join(env.save_dir, f'iter{iteration}.pth'))
+
+        utils.save_state_dict(model, optimizer, iteration, os.path.join(env.save_dir, 'latest.pth'))
+
+
+if __name__ == '__main__':
+    main()
